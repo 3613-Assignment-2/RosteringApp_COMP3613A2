2de9b115dac300eca44b7e147e3856bc3bca6cc4
Cid Wilson Bellamy <142268723+CidWB@users.noreply.github.com>
2025-10-23 02:12:10 -0400

    Update timeentry.py
    Tried to find a workaround for the primary db key (id)

--- App/models/timeentry.py
+++ App/models/timeentry.py
@@ -8,6 +8,14 @@ class TimeEntry(db.Model):
     time_in = db.Column(db.Time, nullable=True)
     time_out = db.Column(db.Time, nullable=True)
 
+    def __init__(self, shift_id, time_in=None, time_out=None):
+        self.shift_id = shift_id
+        self.time_in = time_in
+        self.time_out = time_out   
+
+    def set_id(self, id):
+        self.id = id
+
     def get_json(self):
         return {
             'id': self.id,
@@ -15,9 +23,3 @@ class TimeEntry(db.Model):
             'timeIn': self.time_in,
             'timeOut': self.time_out
         }       
-    
-    def __init__(self, shift_id, time_in=None, time_out=None):
-        self.shift_id = shift_id
-        self.time_in = time_in
-        self.time_out = time_out   
-
